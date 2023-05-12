f3643ae362fa9fa9aedc07ed4a83e619bd7971cb
Mayank <mayankgautam98@gmail.com>
2023-05-12 23:06:34 +0530

    added cors middelware to the server

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const apiV1Router = require("./routes/api-v1");
 
 const app = express();
 const port = 4420;
 
+app.use(cors());
 app.use('/api/v1', apiV1Router);
 
 app.get("/", (req, res) => {
