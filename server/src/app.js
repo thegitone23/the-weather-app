863b0a0aa458877eb96b66a0834e08749afb36e3
Mayank <mayankgautam98@gmail.com>
2023-05-14 03:56:27 +0530

    updated log statements

--- server/src/app.js
+++ server/src/app.js
@@ -6,14 +6,14 @@ const app = express();
 const port = 4420;
 
 app.use(cors());
-app.use('/api/v1', apiV1Router);
+app.use("/api/v1", apiV1Router);
 
 app.get("/", (req, res) => {
   res.json({
-    type: 'success'
-  })
+    type: "success",
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
