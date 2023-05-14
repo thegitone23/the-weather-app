7593d4d2f6491376728a332f8c170c1ceb651dc9
Mayank <mayankgautam98@gmail.com>
2023-05-14 17:27:37 +0530

    updated date formatting

--- client/src/components/WeeksForecast.js
+++ client/src/components/WeeksForecast.js
@@ -1,36 +1,48 @@
-import { weatherFromCode } from "../utils/weather";
+import { weatherFromCode, celsiusToFahrenheit } from "../utils/weather";
 
 export function WeeksForecast({ weeksData }) {
   return (
     <>
       {weeksData && (
         <>
-        <h3>Next 7 Days</h3>
-        <table>
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Min Temprature (Celsius)</th>
-              <th>Max Temprature (Celsius)</th>
-              <th>Forecast</th>
-            </tr>
-          </thead>
+          <h3>Next 7 Days</h3>
+          <table>
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Min Temprature</th>
+                <th>Max Temprature</th>
+                <th>Forecast</th>
+              </tr>
+            </thead>
 
-          <tbody>
-            {(new Array(7).fill(0)).map((_element, index) => {
-              return (
-                <>
-                  <tr>
-                    <td>{weeksData.time[index]}</td>
-                    <td>{weeksData["temperature_2m_min"][index]}</td>
-                    <td>{weeksData["temperature_2m_max"][index]}</td>
-                    <td>{weatherFromCode(weeksData.weathercode[index])}</td>
-                  </tr>
-                </>
-              );
-            })}
-          </tbody>
-        </table>
+            <tbody>
+              {new Array(7).fill(0).map((_element, index) => {
+                return (
+                  <>
+                    <tr>
+                      <td>{weeksData.time[index]}</td>
+                      <td>
+                        {weeksData["temperature_2m_min"][index]} &#8451; /{" "}
+                        {celsiusToFahrenheit(
+                          weeksData["temperature_2m_min"][index]
+                        )}{" "}
+                        &#8457;{" "}
+                      </td>
+                      <td>
+                        {weeksData["temperature_2m_max"][index]} &#8451; /{" "}
+                        {celsiusToFahrenheit(
+                          weeksData["temperature_2m_max"][index]
+                        )}{" "}
+                        &#8457;{" "}
+                      </td>
+                      <td>{weatherFromCode(weeksData.weathercode[index])}</td>
+                    </tr>
+                  </>
+                );
+              })}
+            </tbody>
+          </table>
         </>
       )}
     </>
